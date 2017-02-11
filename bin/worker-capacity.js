74bb9bd010feb1d9e4e2f0ded38e31c656d6bb34
Ryan Clark <ryan.clark.j@gmail.com>
2017-02-11 16:10:15 -0700

    sketch new cli

--- bin/worker-capacity.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var argv = process.argv.slice(2);
-var run = require('../lib/capacity').run;
-
-run(argv, (err, result) => {
-  if (err) console.log('\n%s\n', err);
-  if (result.capacity) console.log('\n%s currently has enough space for an additional %s %s workers.\n', result.cluster, result.capacity, result.stack);
-});
