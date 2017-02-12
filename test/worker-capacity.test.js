1846fb228fa08896bc0037e3f5b3cce39417550e
Ryan Clark <ryan.clark.j@gmail.com>
2017-02-12 17:35:13 -0600

    test fixes

--- test/worker-capacity.test.js
+++ test/worker-capacity.test.js
@@ -1,22 +1,22 @@
 var AWS = require('@mapbox/mock-aws-sdk-js');
-var file = require('../lib/capacity');
+var capacity = require('../lib/worker-capacity');
 var fixtures = require('./fixtures/capacity');
 var test = require('tape');
 
-var argv = { region: 'us-east-1', stack: 'cats-api-staging' };
+var argv = { region: 'us-east-1', stackName: 'cats-api-staging' };
 var cluster = 'arn:aws:ecs:us-east-1:123456789000:cluster/some-cluster-Cluster-000000000000';
 var error = 'some error';
 
 test('[capacity] run - missing region', (assert) => {
-  file.run(['cats-api-staging'], (err) => {
-    assert.equal(err, 'Usage:   worker-capacity <region> <stack_name>\nExample: worker-capacity us-east-1 cats-api-staging');
+  capacity({ stackName: 'cats-api-staging' }, (err) => {
+    assert.equal(err.message, 'No region specified');
     assert.end();
   });
 });
 
 test('[capacity] run - missing stack', (assert) => {
-  file.run(['us-east-1'], (err) => {
-    assert.equal(err, 'Usage:   worker-capacity <region> <stack_name>\nExample: worker-capacity us-east-1 cats-api-staging');
+  capacity({ region: 'us-east-1' }, (err) => {
+    assert.equal(err.message, 'No stack name provided');
     assert.end();
   });
 });
@@ -38,7 +38,7 @@ test('[capacity] run', (assert) => {
   describeContainerInstances.onCall(0).yields(null, fixtures.describeContainerInstances0);
   describeContainerInstances.onCall(1).yields(null, fixtures.describeContainerInstances1);
 
-  file.run(['us-east-1', 'cats-api-staging'], (err, res) => {
+  capacity({ region: 'us-east-1', stackName: 'cats-api-staging' }, (err, res) => {
     assert.ifError(err, 'should not error');
     assert.deepEqual(res, { capacity: 256, cluster: 'some-cluster-Cluster-000000000000', stack: 'cats-api-staging' });
     AWS.CloudFormation.restore();
@@ -50,8 +50,8 @@ test('[capacity] run', (assert) => {
 test('[capacity] getClusterArn - describeStacks error', (assert) => {
   var describeStacks = AWS.stub('CloudFormation', 'describeStacks').yields(error);
 
-  file.getClusterArn(argv, (err) => {
-    assert.deepEqual(describeStacks.firstCall.args[0], { StackName: argv.stack });
+  capacity.getClusterArn(argv, (err) => {
+    assert.deepEqual(describeStacks.firstCall.args[0], { StackName: argv.stackName });
     assert.equal(err.message, error);
     AWS.CloudFormation.restore();
     assert.end();
@@ -61,8 +61,8 @@ test('[capacity] getClusterArn - describeStacks error', (assert) => {
 test('[capacity] getClusterArn', (assert) => {
   var describeStacks = AWS.stub('CloudFormation', 'describeStacks').yields(null, fixtures.describeStacks);
 
-  file.getClusterArn(argv, (err, res) => {
-    assert.deepEqual(describeStacks.firstCall.args[0], { StackName: argv.stack });
+  capacity.getClusterArn(argv, (err, res) => {
+    assert.deepEqual(describeStacks.firstCall.args[0], { StackName: argv.stackName });
     assert.ifError(err, 'should not error');
     assert.equal(res, fixtures.describeStacks.Stacks[0].Outputs[0].OutputValue);
     AWS.CloudFormation.restore();
@@ -74,8 +74,8 @@ test('[capacity] getReservations - describeStackResources error', (assert) => {
   var describeStackResources = AWS.stub('CloudFormation', 'describeStackResources').yields(error);
   var describeTaskDefinition = AWS.stub('ECS', 'describeTaskDefinition');
 
-  file.getReservations(argv, (err) => {
-    assert.deepEqual(describeStackResources.firstCall.args[0], { StackName: argv.stack });
+  capacity.getReservations(argv, (err) => {
+    assert.deepEqual(describeStackResources.firstCall.args[0], { StackName: argv.stackName });
     assert.equal(describeTaskDefinition.called, false, 'ecs.describeTaskDefinition should not be called');
     assert.equal(err.message, error);
     AWS.CloudFormation.restore();
@@ -88,8 +88,8 @@ test('[capacity] getReservations - describeTaskDefinition error', (assert) => {
   var describeStackResources = AWS.stub('CloudFormation', 'describeStackResources').yields(null, fixtures.describeStackResources);
   var describeTaskDefinition = AWS.stub('ECS', 'describeTaskDefinition').yields(error);
 
-  file.getReservations(argv, (err) => {
-    assert.deepEqual(describeStackResources.firstCall.args[0], { StackName: argv.stack });
+  capacity.getReservations(argv, (err) => {
+    assert.deepEqual(describeStackResources.firstCall.args[0], { StackName: argv.stackName });
     assert.deepEqual(describeTaskDefinition.firstCall.args[0], { taskDefinition: fixtures.describeStackResources.StackResources[0].PhysicalResourceId });
     assert.equal(err.message, error);
     AWS.CloudFormation.restore();
@@ -102,8 +102,8 @@ test('[capacity] getReservations - soft memory', (assert) => {
   var describeStackResources = AWS.stub('CloudFormation', 'describeStackResources').yields(null, fixtures.describeStackResources);
   var describeTaskDefinition = AWS.stub('ECS', 'describeTaskDefinition').yields(null, fixtures.describeTaskDefinitionSoftMem);
 
-  file.getReservations(argv, (err, res) => {
-    assert.deepEqual(describeStackResources.firstCall.args[0], { StackName: argv.stack });
+  capacity.getReservations(argv, (err, res) => {
+    assert.deepEqual(describeStackResources.firstCall.args[0], { StackName: argv.stackName });
     assert.deepEqual(describeTaskDefinition.firstCall.args[0], { taskDefinition: fixtures.describeStackResources.StackResources[0].PhysicalResourceId });
     assert.ifError(err, 'should not error');
     assert.deepEqual(res, { Memory: 128, Cpu: 256 });
@@ -117,8 +117,8 @@ test('[capacity] getReservations - hard memory', (assert) => {
   var describeStackResources = AWS.stub('CloudFormation', 'describeStackResources').yields(null, fixtures.describeStackResources);
   var describeTaskDefinition = AWS.stub('ECS', 'describeTaskDefinition').yields(null, fixtures.describeTaskDefinitionHardMem);
 
-  file.getReservations(argv, (err, res) => {
-    assert.deepEqual(describeStackResources.firstCall.args[0], { StackName: argv.stack });
+  capacity.getReservations(argv, (err, res) => {
+    assert.deepEqual(describeStackResources.firstCall.args[0], { StackName: argv.stackName });
     assert.deepEqual(describeTaskDefinition.firstCall.args[0], { taskDefinition: fixtures.describeStackResources.StackResources[0].PhysicalResourceId });
     assert.ifError(err, 'should not error');
     assert.deepEqual(res, { Memory: 128, Cpu: 256 });
@@ -132,8 +132,8 @@ test('[capacity] getReservations - both memories', (assert) => {
   var describeStackResources = AWS.stub('CloudFormation', 'describeStackResources').yields(null, fixtures.describeStackResources);
   var describeTaskDefinition = AWS.stub('ECS', 'describeTaskDefinition').yields(null, fixtures.describeTaskDefinitionBothMem);
 
-  file.getReservations(argv, (err, res) => {
-    assert.deepEqual(describeStackResources.firstCall.args[0], { StackName: argv.stack });
+  capacity.getReservations(argv, (err, res) => {
+    assert.deepEqual(describeStackResources.firstCall.args[0], { StackName: argv.stackName });
     assert.deepEqual(describeTaskDefinition.firstCall.args[0], { taskDefinition: fixtures.describeStackResources.StackResources[0].PhysicalResourceId });
     assert.ifError(err, 'should not error');
     assert.deepEqual(res, { Memory: 128, Cpu: 256 });
@@ -151,7 +151,7 @@ test('[capacity] listInstances - listContainerInstances error', (assert) => {
   });
   var describeContainerInstances = AWS.stub('ECS', 'describeContainerInstances');
 
-  file.listInstances(argv, cluster, (err) => {
+  capacity.listInstances(argv, cluster, (err) => {
     assert.deepEqual(listContainerInstances.firstCall.args, [{ cluster: cluster }]);
     assert.equal(describeContainerInstances.called, false, 'should not have called ecs.describeContainerInstances');
     assert.equal(err.message, error);
@@ -170,7 +170,7 @@ test('[capacity] listInstances - describeContainerInstances error', (assert) => 
   });
   var describeContainerInstances = AWS.stub('ECS', 'describeContainerInstances').yields(error);
 
-  file.listInstances(argv, cluster, (err) => {
+  capacity.listInstances(argv, cluster, (err) => {
     assert.deepEqual(listContainerInstances.firstCall.args[0], { cluster: cluster });
     assert.deepEqual(describeContainerInstances.firstCall.args[0], { cluster: cluster, containerInstances: fixtures.listContainerInstances.containerInstanceArns });
     assert.equal(err.message, error);
@@ -189,7 +189,7 @@ test('[capacity] listInstances - single page', (assert) => {
   });
   var describeContainerInstances = AWS.stub('ECS', 'describeContainerInstances').yields(null, fixtures.describeContainerInstances);
 
-  file.listInstances(argv, cluster, (err, res) => {
+  capacity.listInstances(argv, cluster, (err, res) => {
     assert.deepEqual(listContainerInstances.firstCall.args[0], { cluster: cluster });
     assert.deepEqual(describeContainerInstances.firstCall.args[0], { cluster: cluster, containerInstances: listContainerInstances.containerInstanceArns });
     assert.ifError(err, 'should not error');
@@ -216,7 +216,7 @@ test('[capacity] listInstances - multiple pages', (assert) => {
   describeContainerInstances.onCall(0).yields(null, fixtures.describeContainerInstances0);
   describeContainerInstances.onCall(1).yields(null, fixtures.describeContainerInstances1);
 
-  file.listInstances(argv, cluster, (err, res) => {
+  capacity.listInstances(argv, cluster, (err, res) => {
     assert.deepEqual(listContainerInstances.firstCall.args[0], { cluster: cluster });
     assert.deepEqual(describeContainerInstances.firstCall.args[0], {
       cluster: 'arn:aws:ecs:us-east-1:123456789000:cluster/some-cluster-Cluster-000000000000',
@@ -241,8 +241,8 @@ test('[capacity] calculateRoom - no room', (assert) => {
   var noCpu = [[{ name: 'CPU', integerValue: '0' }, { name: 'MEMORY', integerValue: '1000' }]];
   var noMem = [[{ name: 'CPU', integerValue: '1000' }, { name: 'MEMORY', integerValue: '0' }]];
 
-  assert.equal(file.calculateRoom(noCpu, reservation), 0, 'should return room for 0 workers if no CPU remaining');
-  assert.equal(file.calculateRoom(noMem, reservation), 0, 'should return room for 0 workers if no memory remaining');
+  assert.equal(capacity.calculateRoom(noCpu, reservation), 0, 'should return room for 0 workers if no CPU remaining');
+  assert.equal(capacity.calculateRoom(noMem, reservation), 0, 'should return room for 0 workers if no memory remaining');
   assert.end();
 });
 
@@ -254,7 +254,7 @@ test('[capacity] calculateRoom - room', (assert) => {
     [{ name: 'CPU', integerValue: '60000' }, { name: 'MEMORY', integerValue: '50000' }]
   ];
 
-  var result = file.calculateRoom(resources, reservation);
+  var result = capacity.calculateRoom(resources, reservation);
   assert.equal(result, 235, 'should equal sum of tasks per instance based on most limited resource');
   assert.end();
 });
