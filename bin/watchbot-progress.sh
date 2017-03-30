be2fd4bdea767c439fd0374c83ca9ebcc7c15707
Sam Matthews <matthews.sam@gmail.com>
2017-03-30 12:42:34 -0700

    quote progress args (#142)

--- bin/watchbot-progress.sh
+++ bin/watchbot-progress.sh
@@ -5,4 +5,4 @@ set -e
 progress=$(node -e "console.log(require.resolve('@mapbox/watchbot-progress'));")
 base=$(dirname ${progress})
 
-${base}/bin/watchbot-progress.js $@
+${base}/bin/watchbot-progress.js "$@"
