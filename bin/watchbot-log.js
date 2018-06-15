fa521405cba32deb41f60338f512916bba79e172
jakepruitt <jake@mapbox.com>
2018-06-15 13:33:01 -0700

    Add watchbot-log

---
+++ bin/watchbot-log.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+/**
+ * watchbot-log "something that you want logged"
+ *   - or -
+ * echo "somehing that you want logged" | watchbot-log
+ */
+
+const Logger = require('..').Logger;
+const args = process.argv.slice(2);
+
+const logger = new Logger('worker');
+
+if (args[0]) {
+  return logger.log(args[0]);
+}
+
+process.stdin.pipe(logger.stream());
