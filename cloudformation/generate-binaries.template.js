d960a69e8cfb1536848bbd6f8ae983dad487acde
arunasank <aruna@mapbox.com>
2018-06-27 16:45:55 +0530

    remove unnecessary binary

---
+++ cloudformation/generate-binaries.template.js
@@ -0,0 +1,176 @@
+'use strict';
+
+const redent = require('redent');
+const cf = require('@mapbox/cloudfriend');
+
+const Parameters = {
+  GitSha: { Type: 'String' }
+};
+
+const Resources = {
+  BundlerLogs: {
+    Type: 'AWS::Logs::LogGroup',
+    Properties: {
+      LogGroupName: cf.sub('/aws/codebuild/${AWS::StackName}-bundler'),
+      RetentionInDays: 14
+    }
+  },
+  BundlerRole: {
+    Type: 'AWS::IAM::Role',
+    Properties: {
+      AssumeRolePolicyDocument: {
+        Statement: [
+          {
+            Effect: 'Allow',
+            Action: 'sts:AssumeRole',
+            Principal: { Service: 'codebuild.amazonaws.com' }
+          }
+        ]
+      },
+      Policies: [
+        {
+          PolicyName: cf.ref('GitSha'),
+          PolicyDocument: {
+            Statement: [
+              {
+                Effect: 'Allow',
+                Action: 'logs:*',
+                Resource: cf.getAtt('BundlerLogs', 'Arn')
+              },
+              {
+                Effect: 'Allow',
+                Action: [
+                  's3:ListBucket',
+                  's3:ListObjects',
+                  's3:GetObject',
+                  's3:HeadObject',
+                  's3:PutObject',
+                  's3:PutObjectAcl'
+                ],
+                Resource: [
+                  cf.sub('arn:${AWS::Partition}:s3:::watchbot-binaries'),
+                  cf.sub('arn:${AWS::Partition}:s3:::watchbot-binaries/*')
+                ]
+              }
+            ]
+          }
+        }
+      ]
+    }
+  },
+  Bundler: {
+    Type: 'AWS::CodeBuild::Project',
+    Properties: {
+      Name: cf.sub('${AWS::StackName}-bundler'),
+      Description: 'Uploads code-pipeline-helper bundles',
+      Artifacts: {
+        Type: 'CODEPIPELINE'
+      },
+      Environment: {
+        Type: 'LINUX_CONTAINER',
+        ComputeType: 'BUILD_GENERAL1_SMALL',
+        Image: 'aws/codebuild/nodejs:8.11.0'
+      },
+      ServiceRole: cf.getAtt('BundlerRole', 'Arn'),
+      Source: {
+        Type: 'CODEPIPELINE',
+        BuildSpec: redent(`
+          version: 0.2
+          phases:
+            install:
+              commands:
+                - npm install -g npm@5.8.0
+                - npm ci --production
+            build:
+              commands:
+                - node bin/watchbot-binary-generator
+        `)
+      }
+    }
+  },
+  PipelineRole: {
+    Type: 'AWS::IAM::Role',
+    Properties: {
+      AssumeRolePolicyDocument: {
+        Statement: [
+          {
+            Effect: 'Allow',
+            Action: 'sts:AssumeRole',
+            Principal: { Service: 'codepipeline.amazonaws.com' }
+          }
+        ]
+      },
+      Policies: [
+        {
+          PolicyName: 'main',
+          PolicyDocument: {
+            Statement: [
+              {
+                Effect: 'Allow',
+                Action: [
+                  's3:ListBucket',
+                  's3:GetBucketVersioning',
+                  's3:GetObject',
+                  's3:GetObjectVersion',
+                  's3:PutObject'
+                ],
+                Resource: [
+                  cf.sub('arn:${AWS::Partition}:s3:::watchbot-binaries'),
+                  cf.sub('arn:${AWS::Partition}:s3:::watchbot-binaries/*')
+                ]
+              },
+              {
+                Effect: 'Allow',
+                Action: [
+                  'codebuild:StartBuild',
+                  'codebuild:BatchGetBuilds',
+                  'iam:PassRole'
+                ],
+                Resource: '*'
+              }
+            ]
+          }
+        }
+      ]
+    }
+  },
+  Pipeline: {
+    Type: 'Custom::CodePipelineHelper',
+    Properties: {
+      ServiceToken: cf.importValue('code-pipeline-helper-production-custom-resource'),
+      Owner: 'mapbox',
+      Repo: 'ecs-watchbot',
+      Branch: 'random',
+      Name: cf.stackName,
+      RoleArn: cf.getAtt('PipelineRole', 'Arn'),
+      ArtifactStore: {
+        Type: 'S3',
+        Location: 'watchbot-binaries'
+      },
+      Stages: [
+        {
+          Name: 'Bundle',
+          Actions: [
+            {
+              Name: 'Bundle',
+              ActionTypeId: {
+                Category: 'Build',
+                Owner: 'AWS',
+                Version: '1',
+                Provider: 'CodeBuild'
+              },
+              InputArtifacts: [
+                { Name: 'Source' }
+              ],
+              Configuration: {
+                ProjectName: cf.ref('Bundler')
+              }
+            }
+          ]
+        }
+      ]
+    }
+  }
+};
+
+module.exports = cf.merge({ Parameters, Resources });
