adcb236a7744e74bda925f11bf0f6bdd1a859daf
jakepruitt <jake@mapbox.com>
2018-06-27 10:47:31 -0700

    Only expose template, which can run on node 6

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 'use strict';
 
 module.exports = {
-  template: require('./lib/template'),
-  Messages: require('./lib/messages'),
-  Message: require('./lib/message'),
-  Worker: require('./lib/worker'),
-  Watcher: require('./lib/watcher'),
-  Logger: require('./lib/logger')
+  template: require('./lib/template')
 };
