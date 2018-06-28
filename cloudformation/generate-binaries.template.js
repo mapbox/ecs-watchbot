61358da0a507092380744a7c06e118b4cc19070e
arunasank <aruna@mapbox.com>
2018-06-28 18:19:05 +0530

    remove generate-binaries

--- cloudformation/generate-binaries.template.js
+++ cloudformation/generate-binaries.template.js
@@ -81,7 +81,6 @@ const Resources = {
                 - npm ci --production
             build:
               commands:
-                - chmod +x bin/generate-binaries
                 - node bin/watchbot-binary-generator
         `)
       }
