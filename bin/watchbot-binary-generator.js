f6fe20f9ba8651def678d863d2883c92d8d40789
arunasank <aruna@mapbox.com>
2018-06-28 07:28:39 +0530

    modify to read tags from the package.json

--- bin/watchbot-binary-generator.js
+++ bin/watchbot-binary-generator.js
@@ -7,6 +7,8 @@ const cp = require('child_process');
 const util = require('util');
 const AWS = require('aws-sdk');
 const exec = util.promisify(cp.exec);
+const path = require('path');
+const pkg = require(path.resolve(__dirname, '..'));
 
 const uploadBundle = async () => {
   const s3 = new AWS.S3();
@@ -19,30 +21,22 @@ const uploadBundle = async () => {
   };
 
   console.log('Generating the binaries from ecs-watchbot');
-  await exec('./generate-binaries', { cwd: __dirname });
+  await exec('npm ci --production', { cwd: '.' });
+  await exec('npm install -g pkg', { cwd: '.' });
+  await exec('pkg .', { cwd: '.' });
 
-  const sha = (await exec('git rev-parse HEAD', { cwd: `${__dirname}/ecs-watchbot` })).stdout.trim();
-  let version;
-  try {
-    version = (await exec('git describe --tags --exact-match'), { cwd: `${__dirname}/ecs-watchbot` }).stdout.trim();
-  } catch (err) {
-    console.log(`No tag found for ${sha}. Not creating a tag specific watchbot binary on S3.`);
-    version = false;
-  }
-  if (version) {
-    console.log(`Tag ${version} found for ${sha}. Creating a tag specific watchbot binary in S3.`);
-    prefix.forEach(async (pre) => {
-      console.log(`Uploaded bundle to s3://${Bucket}/${pre}/${version}/watchbot`);
-      await s3.putObject({
-        Bucket,
-        Key: `${pre}/${version}/watchbot`,
-        Body: fs.createReadStream(`${__dirname}/ecs-watchbot/${pkgNames[pre]}`),
-        ACL: 'public-read'
-      }).promise();
-    });
-  }
+  prefix.forEach(async (pre) => {
+    console.log(`Uploaded bundle to s3://${Bucket}/${pre}/${pkg.version}/watchbot`);
+    await s3.putObject({
+      Bucket,
+      Key: `${pre}/${pkg.version}/watchbot`,
+      Body: fs.createReadStream(pkgNames[pre]),
+      ACL: 'public-read'
+    }).promise();
+  });
   console.log('Fin.');
 };
+
 
 if (require.main === module) {
   uploadBundle()
