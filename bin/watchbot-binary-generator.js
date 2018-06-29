e8b205ea6321291751d4777861b6e2beb3865e96
arunasank <aruna@mapbox.com>
2018-06-29 13:42:41 +0530

    use git ls-remote directly

--- bin/watchbot-binary-generator.js
+++ bin/watchbot-binary-generator.js
@@ -7,19 +7,17 @@ const cp = require('child_process');
 const util = require('util');
 const AWS = require('aws-sdk');
 const exec = util.promisify(cp.exec);
-const remoteGitTags = require('remote-git-tags');
 
-const getTagForSha = (sha) => {
-  return new Promise((resolve, reject) => {
-    remoteGitTags('github.com/mapbox/ecs-watchbot')
-      .then((allTags) => {
-        allTags.forEach((remoteSha, remoteTag) => {
-          if (remoteSha === sha)
-            resolve(remoteTag);
-        });
-        resolve(null);
-      })
-      .catch((err) => reject(err));
+const getTagForSha = async (sha) => {
+  return new Promise(async (resolve, reject) => {
+    const data = (await exec('git ls-remote --tags https://github.com/mapbox/ecs-watchbot')).stdout.split('\n');
+    if (data.stderr) reject(data.stderr);
+    data.forEach((ref) => {
+      if (ref[0] !== sha) return;
+      ref = ref.split('\t');
+      const tagRegex = /refs\/tags\/(v[0-9.-]+)(\^\{(.*)\})*/;
+      return resolve(tagRegex.exec(ref[1])[1]);
+    });
   });
 };
 
@@ -38,22 +36,20 @@ const uploadBundle = async () => {
   await exec('pkg .');
   const sha = process.env.CODEBUILD_RESOLVED_SOURCE_VERSION;
 
-  getTagForSha(sha)
-    .then((tag) => {
-      if (tag) {
-        prefix.forEach(async (pre) => {
-          console.log(`Uploading the package to s3://${Bucket}/${pre}/${tag}/watchbot`);
-          await s3.putObject({
-            Bucket,
-            Key: `${pre}/${tag}/watchbot`,
-            Body: fs.createReadStream(pkgNames[pre]),
-            ACL: 'public-read'
-          }).promise();
-        });
-      } else {
-        console.log(`No tag found for ${process.env.CODEBUILD_RESOLVED_SOURCE_VERSION}`);
-      }
+  const tag = await getTagForSha(sha);
+  if (tag) {
+    prefix.forEach(async (pre) => {
+      console.log(`Uploading the package to s3://${Bucket}/${pre}/${tag}/watchbot`);
+      await s3.putObject({
+        Bucket,
+        Key: `${pre}/${tag}/watchbot`,
+        Body: fs.createReadStream(pkgNames[pre]),
+        ACL: 'public-read'
+      }).promise();
     });
+  } else {
+    console.log(`No tag found for ${process.env.CODEBUILD_RESOLVED_SOURCE_VERSION}`);
+  }
 };
 
 if (require.main === module) {
