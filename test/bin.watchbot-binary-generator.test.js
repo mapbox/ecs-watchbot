385f7f5ce49931f2429020470c7e27b9253d97c9
arunasank <aruna@mapbox.com>
2018-06-29 14:37:46 +0530

    add test

---
+++ test/bin.watchbot-binary-generator.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const test = require('tape');
+const wbg = require('../bin/watchbot-binary-generator');
+const AWS = require('@mapbox/mock-aws-sdk-js');
+const lib = require('../bin/watchbot-binary-generator.js');
+
+tape('getTagForSha: commit found', async (assert) => {
+	const tagSha = 'f4815eb9f3bcfba88930bbe12d0888254af7cfa6';
+	const
+});
