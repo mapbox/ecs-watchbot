c85ec6430916156c20f101893dfd83f6df197909
Tapasweni Pathak <tapaswenipathak@gmail.com>
2018-07-05 01:21:52 +0530

    Fixes deadletter related tickets (#220)
    * list urls of a stack
    
    * async/await dead-letter
    
    * and deadletter
    
    * eslint
    
    * eslint
    
    * classed node8ed some watchbot-dead-letter test
    
    * use strict, run as separate node script
    
    * remove class, only async/await functions
    
    * consistent async/await handling for each function
    
    * add better error handling
    
    * remove test file to check for travis build
    
    * update jest
    
    * update jest
    
    * add some tests which doesn't work
    
    * error
    
    * some better async/await handling for tests
    
    * current state
    
    * Attempt to get deadletter CLI ready
    
    * Fix all tests for dead letter CLI
    
    * add some strictness
    
    * Remove bin.cli.js
    
    * Remove more unnecessary changes
    
    * :/
    
    * Get tests to work
    
    * Fix the last remaining errors

---
+++ bin/dead-letter.js
@@ -0,0 +1,432 @@
+#!/usr/bin/env node
+'use strict';
+
+/* eslint-disable no-console */
+
+const AWS = require('aws-sdk');
+const inquirer = require('inquirer');
+const stream = require('stream');
+const Queue = require('p-queue');
+const Spinner = require('cli-spinner').Spinner;
+const cwlogs = require('cwlogs');
+const meow = require('meow');
+const split = require('binary-split');
+
+const main = async () => {
+  const cli = meow({
+    help: `
+      USAGE: watchbot-dead-letter [OPTIONS]
+      Options:
+        -h, --help          show this help message
+        -s, --stack-name    the full name of a watchbot stack
+        -r, --region        the region of the stack (default us-east-1)
+    `,
+    description: 'Helper utilities for interacting with watchbot dead letter queues'
+  }, {
+    flags: {
+      stackName: { alias: 's' },
+      region: { alias: 'r', default: 'us-east-1' }
+    }
+  });
+  cli.flags.stackName = cli.flags.stackName || cli.flags.s;
+  cli.flags.region = cli.flags.region || cli.flags.r || 'us-east-1';
+
+  if (!cli.flags.stackName) cli.showHelp();
+
+  const sqs = new AWS.SQS({ region: cli.flags.region });
+  const cfn = new AWS.CloudFormation({ region: cli.flags.region });
+
+  const actions = { purge, writeOut, replay, triage };
+
+  try {
+    const queues = await findQueues(cfn, cli.flags);
+    const queue = await selectQueue(queues);
+    const data = await triageSelection(queue);
+
+    await actions[data.action](sqs, data.queue);
+  } catch (err) {
+    throw err;
+  }
+};
+
+async function findQueues(cfn, options) {
+  const res = await cfn.describeStacks({ StackName: options.stackName }).promise();
+  if (!res.Stacks[0]) {
+    throw new Error(`Could not find ${options.stackName} in ${options.region}`);
+  }
+  const deadLetterQueues = res.Stacks[0].Outputs
+    .filter((o) => /DeadLetterQueueUrl/.test(o.OutputKey))
+    .map((o) => ({
+      prefix: o.OutputKey.replace('DeadLetterQueueUrl', ''),
+      url: o.OutputValue
+    }));
+
+  const workQueues = res.Stacks[0].Outputs
+    .filter((o) => /QueueUrl/.test(o.OutputKey) && !/DeadLetterQueueUrl/.test(o.OutputKey))
+    .map((o) => ({
+      prefix: o.OutputKey.replace('QueueUrl', ''),
+      url: o.OutputValue
+    }));
+  const logGroups = res.Stacks[0].Outputs
+    .filter((o) => /LogGroup/.test(o.OutputKey))
+    .map((o) => ({
+      prefix: o.OutputKey.replace('LogGroup', ''),
+      arn: o.OutputValue
+    }));
+
+  return { deadLetterQueues, workQueues, logGroups };
+}
+
+async function selectQueue(queues) {
+  if (queues.deadLetterQueues.length === 1) return {
+    deadLetter: queues.deadLetterQueues[0].url,
+    work: queues.workQueues.find((queue) => queue.prefix === queues.deadLetterQueues[0].prefix).url,
+    logs: queues.logGroups.find((group) => group.prefix === queues.deadLetterQueues[0].prefix).arn
+  };
+
+  const answers = await inquirer.prompt({
+    type: 'list',
+    name: 'queue',
+    message: 'Which queue would you like to triage?',
+    choices: queues.deadLetterQueues.map((queue) => queue.prefix)
+  });
+
+  const deadLetterQueue = queues.deadLetterQueues.find((queue) => queue.prefix === answers.queue);
+
+  return {
+    deadLetter: deadLetterQueue.url,
+    work: queues.workQueues.find((queue) => queue.prefix === deadLetterQueue.prefix).url,
+    logs: queues.logGroups.find((group) => group.prefix === deadLetterQueue.prefix).arn
+  };
+}
+
+async function triageSelection(queue) {
+  const answers = await inquirer.prompt({
+    type: 'list',
+    name: 'action',
+    message: 'Would you like to:',
+    choices: [
+      'Triage dead messages individually?',
+      'Print out all dead messages?',
+      'Return all dead messages to the work queue?',
+      'Purge the dead letter queue?'
+    ]
+  });
+
+  const mapping = {
+    'Purge the dead letter queue?': 'purge',
+    'Return all dead messages to the work queue?': 'replay',
+    'Triage dead messages individually?': 'triage',
+    'Print out all dead messages?': 'writeOut'
+  };
+
+  return { queue, action: mapping[answers.action] };
+}
+
+async function purge(sqs, queue) {
+  const answers = await inquirer.prompt({
+    type: 'confirm',
+    name: 'purge',
+    message: 'You are about to remove all jobs from the dead letter queue permanently. Are you sure?'
+  });
+
+  if (answers.purge)
+    return await sqs.purgeQueue({ QueueUrl: queue.deadLetter }).promise();
+
+  return Promise.resolve();
+}
+
+async function writeOut(sqs, queue) {
+  const reciever = receiveAll(sqs, queue.deadLetter);
+
+  const stringifier = new stream.Transform({
+    objectMode: true,
+    transform: function(msg, _, callback) {
+      const data = { subject: msg.subject, message: msg.message };
+      this.push(`${JSON.stringify(data)}\n`);
+      stringifier.handles.push(msg.handle);
+      callback();
+    }
+  });
+  stringifier.handles = [];
+
+  return new Promise((resolve, reject) => {
+    const done = async () => {
+      try {
+        await returnMany(sqs, queue.deadLetter, stringifier.handles);
+        resolve();
+      } catch (e) {
+        reject(e);
+      }
+    };
+
+    reciever
+      .pipe(stringifier)
+      .on('end', done)
+      .pipe(process.stdout);
+  });
+}
+
+async function replay(sqs, queue) {
+  const answers = await inquirer.prompt({
+    type: 'confirm',
+    name: 'replayAll',
+    message: 'You are about to return all messages in the dead letter queue to the work queue. Are you sure?'
+  });
+
+  if (!answers.replayAll) return Promise.resolve();
+
+  const spinner = new Spinner('Returning all dead messages to the work queue...');
+  spinner.setSpinnerString('⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆');
+  spinner.start();
+
+  const reciever = receiveAll(sqs, queue.deadLetter);
+
+  const replayer = new stream.Writable({
+    objectMode: true,
+    write: async function(msg, _, callback) {
+      try {
+        await replayOne(sqs, queue.work, msg);
+        await deleteOne(sqs, queue.deadLetter, msg);
+        callback();
+      } catch (err) {
+        callback(err);
+      }
+    }
+  });
+
+  await new Promise((resolve, reject) => {
+    reciever
+      .on('error', reject)
+      .pipe(replayer)
+      .on('error', reject)
+      .on('finish', resolve);
+  });
+
+  return spinner.stop(true);
+}
+
+async function triage(sqs, queue) {
+  let done = false;
+  while (!done) {
+    done = await triageOne(sqs, queue);
+  }
+}
+
+async function triagePrompts(sqs, queue, message) {
+  const actions = { replayOne, returnOne, deleteOne };
+
+  const answers = await inquirer.prompt({
+    type: 'list',
+    name: 'action',
+    message: 'Would you like to:',
+    choices: [
+      'Return this message to the work queue?',
+      'Return this message to the dead letter queue?',
+      'Delete this message entirely?',
+      'View this message\'s recent logs?',
+      'Stop individual triage?'
+    ]
+  });
+  const mapping = {
+    'Return this message to the work queue?': 'replayOne',
+    'Return this message to the dead letter queue?': 'returnOne',
+    'View this message\'s recent logs?': 'logs',
+    'Delete this message entirely?': 'deleteOne',
+    'Stop individual triage?': 'stop'
+  };
+
+  const choice = mapping[answers.action];
+  const queueUrl = choice === 'replayOne' ? queue.work : queue.deadLetter;
+
+  if (choice === 'logs') {
+    return await getLogs(sqs, queue, message);
+  }
+
+  if (choice === 'stop') {
+    await returnOne(sqs, queueUrl, message);
+    return true;
+  }
+
+  if (choice === 'replayOne') {
+    await replayOne(sqs, queueUrl, message);
+    await deleteOne(sqs, queue.deadLetter, message);
+    return false;
+  }
+
+  return await actions[choice](sqs, queueUrl, message);
+}
+
+async function triageOne(sqs, queue) {
+  const messages = await receive(sqs, 1, queue.deadLetter);
+  const message = messages[0];
+  if (!message) {
+    console.log('');
+    console.log('No messages found');
+    return true;
+  }
+
+  console.log('');
+  console.log(`Subject: ${message.subject}`);
+  console.log(`Message: ${message.message}`);
+
+  return await triagePrompts(sqs, queue, message);
+}
+
+async function receive(sqs, count, queueUrl) {
+  const data = await sqs.receiveMessage({
+    QueueUrl: queueUrl,
+    WaitTimeSeconds: 1,
+    MaxNumberOfMessages: count,
+    VisibilityTimeout: 10 * 60
+  }).promise();
+
+  return (data.Messages || []).map((message) => ({
+    id: message.MessageId,
+    body: message.Body,
+    subject: JSON.parse(message.Body).Subject,
+    message: JSON.parse(message.Body).Message,
+    handle: message.ReceiptHandle
+  }));
+}
+
+
+async function returnOne(sqs, queueUrl, message) {
+  const handle = typeof message === 'string' ? message : message.handle;
+  return await sqs.changeMessageVisibility({
+    QueueUrl: queueUrl,
+    ReceiptHandle: handle,
+    VisibilityTimeout: 0
+  }).promise();
+}
+
+async function returnMany(sqs, queueUrl, handles) {
+  const spinner = new Spinner(`Returning ${handles.length} jobs to the queue...`);
+  spinner.setSpinnerString('⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆');
+  spinner.start();
+
+  const queue = new Queue({ concurrency: 10 });
+  const returns = handles.map((handle) => queue.add(() => returnOne(sqs, queueUrl, handle)));
+  const returnManyResult = await Promise.all(returns);
+  spinner.stop(true);
+  return returnManyResult;
+}
+
+async function replayOne(sqs, queueUrl, message) {
+  return await sqs.sendMessage({
+    QueueUrl: queueUrl,
+    MessageBody: message.body
+  }).promise();
+}
+
+async function deleteOne(sqs, queueUrl, message) {
+  return await sqs.deleteMessage({
+    QueueUrl: queueUrl,
+    ReceiptHandle: message.handle
+  }).promise();
+}
+
+function receiveAll(sqs, queueUrl) {
+  const messages = [];
+  let pending = false;
+  let next = async function() {
+    pending = true;
+    const msgs = await receive(sqs, 10, queueUrl);
+    if (msgs) {
+      pending = false;
+      msgs.forEach((msg) => messages.push(msg));
+      if (!msgs.length) next = null;
+    }
+    return msgs;
+  };
+
+  return new stream.Readable({
+    objectMode: true,
+    read: async function() {
+      let status = true;
+      while (status && messages.length) status = this.push(messages.shift());
+      if (messages.length)  return;
+      if (!next) return this.push(null);
+      if (status && !pending) {
+        try {
+          await next();
+          this._read();
+        } catch (err) {
+          this.emit('error', err);
+        }
+      }
+    }
+  });
+}
+
+async function getLogs(sqs, queue, message) {
+  const spinner = new Spinner('Searching CloudWatch logs...');
+  spinner.setSpinnerString('⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆');
+  spinner.start();
+
+  return new Promise((resolve, reject) => {
+    fetchLogs(queue.logs, message.message, (err, data) => {
+      if (err) return reject(err);
+
+      const re = new RegExp(`\\[watchbot\\] \\[(.*?)\\] {"subject":".*?","message":"${message.message}"`);
+      const line = data.split('\n').find((line) => re.test(line));
+      if (!line) return resolve('Could not find any matching logs\n');
+
+      const id = line.match(re)[1];
+      fetchLogs(queue.logs, id, (err, data) => {
+        if (err) return reject(err);
+        resolve(data);
+      });
+    });
+  }).then(async (data) => {
+    spinner.stop(true);
+    console.log();
+    console.log(data);
+    return await triagePrompts(sqs, queue, message);
+  });
+}
+
+function fetchLogs(logGroup, messageId, callback) {
+  const readable = cwlogs.readable({
+    region: logGroup.split(':')[3],
+    group: logGroup.split(':')[6],
+    pattern: messageId,
+    messages: true,
+    start: Date.now() - 6 * 60 * 60 * 1000
+  }).on('error', callback);
+
+  const writable = new stream.Writable();
+  writable.buffer = [];
+  writable.buffer.current = 0;
+
+  writable.buffer.add = function(line) {
+    if (writable.buffer.current + line.length < 50 * 1024) {
+      writable.buffer.current += line.length;
+      writable.buffer.push(line);
+    } else {
+      const drop = writable.buffer.shift();
+      if (!drop) {
+        const truncate = line.substring(0, 50 * 1024 - 5) + '...';
+        writable.buffer.add(truncate);
+      } else {
+        writable.buffer.current -= drop.length;
+        writable.buffer.add(line);
+      }
+    }
+  };
+
+  writable._write = function(line, enc, callback) {
+    writable.buffer.add(line.toString());
+    callback();
+  };
+
+  writable.on('finish', () => {
+    callback(null, writable.buffer.join('\n') + '\n');
+  }).on('error', callback);
+
+  readable.pipe(split()).pipe(writable);
+}
+
+module.exports = main;
+
+if (require.main === module) main();
---
+++ test/bin.dead-letter.test.js
@@ -0,0 +1,420 @@
+'use strict';
+
+const test = require('tape');
+const sinon = require('sinon');
+const AWS = require('@mapbox/mock-aws-sdk-js');
+const watchbotDeadletter = require('../bin/dead-letter');
+const inquirer = require('inquirer');
+const cwlogs = require('cwlogs');
+const stream = require('stream');
+
+test('[bin.watchbot-dead-letter] stack not found', async (assert) => {
+  process.argv = ['node', 'bin/whatever', '--stack-name', 'stackName', '--region', 'regionName'];
+  process.argv.QueueUrl = 'https://something';
+  AWS.stub('CloudFormation', 'describeStacks', function() {
+    this.request.promise.returns(Promise.resolve({
+      Stacks: []
+    }));
+  });
+
+  try {
+    await watchbotDeadletter();
+  } catch (err) {
+    assert.equal(err.message, 'Could not find stackName in regionName', 'expected error message');
+    AWS.CloudFormation.restore();
+    assert.end();
+  }
+});
+
+test('[dead-letter] individual message triage', async (assert) => {
+  process.argv = ['node', 'bin/whatever', '--stack-name', 'stackName', '--region', 'regionName'];
+  process.argv.QueueUrl = 'https://something';
+  const prompt = sinon.stub(inquirer, 'prompt');
+  prompt.onCall(0).returns(Promise.resolve({ action: 'Triage dead messages individually?' }));
+  prompt.onCall(1).returns(Promise.resolve({ action: 'Return this message to the work queue?' }));
+  prompt.onCall(2).returns(Promise.resolve({ action: 'Return this message to the dead letter queue?' }));
+  prompt.onCall(3).returns(Promise.resolve({ action: 'Delete this message entirely?' }));
+  prompt.onCall(4).returns(Promise.resolve({ action: 'View this message\'s recent logs?' }));
+  prompt.onCall(5).returns(Promise.resolve({ action: 'Stop individual triage?' }));
+  AWS.stub('CloudFormation', 'describeStacks', function() {
+    this.request.promise.returns(Promise.resolve({
+      Stacks: [
+        {
+          Outputs: [
+            { OutputKey: 'oneDeadLetterQueueUrl', OutputValue: 'oneDead' },
+            { OutputKey: 'oneQueueUrl', OutputValue: 'oneWork' },
+            { OutputKey: 'oneLogGroup', OutputValue: 'oneLogs' }
+          ]
+        }
+      ]
+    }));
+  });
+  const receive = AWS.stub('SQS', 'receiveMessage');
+  receive.onCall(0).returns({
+    promise: () => Promise.resolve({ Messages: [{ MessageId: 'id-1', Body: JSON.stringify({ Subject: 'subject-1', Message: 'message-1' }), ReceiptHandle: 'handle-1' }] })
+  });
+  receive.onCall(1).returns({
+    promise: () => Promise.resolve({ Messages: [{ MessageId: 'id-2', Body: JSON.stringify({ Subject: 'subject-2', Message: 'message-2' }), ReceiptHandle: 'handle-2' }] })
+  });
+  receive.onCall(2).returns({
+    promise: () => Promise.resolve({ Messages: [{ MessageId: 'id-3', Body: JSON.stringify({ Subject: 'subject-3', Message: 'message-3' }), ReceiptHandle: 'handle-3' }] })
+  });
+  receive.onCall(3).returns({
+    promise: () => Promise.resolve({ Messages: [{ MessageId: 'id-4', Body: JSON.stringify({ Subject: 'subject-4', Message: 'message-4' }), ReceiptHandle: 'handle-4' }] })
+  });
+
+  const send = AWS.stub('SQS', 'sendMessage', function() {
+    this.request.promise.returns(Promise.resolve());
+  });
+
+  const del = AWS.stub('SQS', 'deleteMessage', function() {
+    this.request.promise.returns(Promise.resolve());
+  });
+  const vis = AWS.stub('SQS', 'changeMessageVisibility', function() {
+    this.request.promise.returns(Promise.resolve());
+  });
+  const fetch = sinon.stub(cwlogs, 'readable');
+  let count = 0;
+  const mockedCwlogs = new stream.Readable({
+    read: function() {
+      if (count === 0) {
+        this.push([
+          '[Sun, 12 Feb 2017 00:24:41 GMT] [watchbot] [a406f47b-a0f2-49a6-a159-b0f8578104bf] {"subject":"bozo","message":"message-4","receives":"1"}',
+          '[Sun, 12 Feb 2017 00:24:42 GMT] [watchbot] [436d13dc-a666-44fd-a2df-70f1f4b3f107] {"subject":"bozo","message":"message-5","receives":"1"}'
+        ].join('\n'));
+        count++;
+      }
+      if (count > 0) this.push('final log') && this.push(null);
+    }
+  });
+  fetch.returns(mockedCwlogs);
+
+  try {
+    await watchbotDeadletter();
+    assert.equal(send.callCount, 1, 'one sendMessage request');
+    assert.ok(send.calledWith({
+      QueueUrl: 'oneWork',
+      MessageBody: JSON.stringify({ Subject: 'subject-1', Message: 'message-1' })
+    }), 'returns the first message to work queue');
+
+    assert.equal(del.callCount, 2, 'two deleteMessage requests');
+    assert.ok(del.calledWith({
+      QueueUrl: 'oneDead',
+      ReceiptHandle: 'handle-1'
+    }), 'deletes the first message from the dead letter queue');
+    assert.ok(del.calledWith({
+      QueueUrl: 'oneDead',
+      ReceiptHandle: 'handle-3'
+    }), 'deletes the third message from the dead letter queue');
+
+    assert.equal(vis.callCount, 2, 'two changeMessageVisibility requests');
+    assert.ok(vis.calledWith({
+      QueueUrl: 'oneDead',
+      ReceiptHandle: 'handle-2',
+      VisibilityTimeout: 0
+    }), 'returns the second message to the dead letter queue');
+    assert.ok(vis.calledWith({
+      QueueUrl: 'oneDead',
+      ReceiptHandle: 'handle-4',
+      VisibilityTimeout: 0
+    }), 'returns the fourth message to the dead letter queue');
+
+    assert.equal(fetch.callCount, 2, 'two calls to fetch recent logs');
+    assert.equals(fetch.args[0][0].pattern, 'message-4', 'one fetch call based on message itself');
+    assert.equals(fetch.args[1][0].pattern, 'a406f47b-a0f2-49a6-a159-b0f8578104bf', 'one fetch call based on message itself');
+
+    prompt.restore();
+    fetch.restore();
+    AWS.CloudFormation.restore();
+    AWS.SQS.restore();
+    assert.end();
+  } catch (err) {
+    assert.ifError(err);
+  }
+});
+
+test('[bin.watchbot-dead-letter] check initial prompts (single watchbot)', async (assert) => {
+  const prompt = sinon.stub(inquirer, 'prompt');
+  prompt.onCall(0).returns(Promise.resolve({ action: 'Purge the dead letter queue?' }));
+  prompt.onCall(1).returns(Promise.resolve({ purge: true }));
+
+  AWS.stub('CloudFormation', 'describeStacks', function() {
+    this.request.promise.returns(Promise.resolve({
+      Stacks: [
+        {
+          Outputs: [
+            { OutputKey: 'oneDeadLetterQueueUrl', OutputValue: 'oneDead' },
+            { OutputKey: 'oneQueueUrl', OutputValue: 'oneWork' },
+            { OutputKey: 'oneLogGroup', OutputValue: 'oneLogs' }
+          ]
+        }
+      ]
+    }));
+  });
+
+  const purge = AWS.stub('SQS', 'purgeQueue', function() {
+    this.request.promise.returns(Promise.resolve());
+  });
+
+  try {
+    await watchbotDeadletter();
+
+    assert.equal(prompt.callCount, 2, 'two prompts');
+
+    assert.equal(prompt.args[0][0].type, 'list', 'first prompt type = list');
+    assert.deepEqual(prompt.args[0][0].choices, [
+      'Triage dead messages individually?',
+      'Print out all dead messages?',
+      'Return all dead messages to the work queue?',
+      'Purge the dead letter queue?'
+    ], 'first prompt expected actions');
+
+    assert.equal(prompt.args[1][0].type, 'confirm', 'second prompt type = confirm');
+
+    assert.equal(purge.callCount, 1, 'calls purgeQueue');
+    assert.ok(purge.calledWith({ QueueUrl: 'oneDead' }), 'purges the dead letter queue');
+
+    prompt.restore();
+    AWS.CloudFormation.restore();
+    AWS.SQS.restore();
+  } catch (err) {
+    assert.ifError(err, 'success');
+  }
+  assert.end();
+});
+
+test('[dead-letter] reject purge confirmation', async (assert) => {
+  const prompt = sinon.stub(inquirer, 'prompt');
+  prompt.onCall(0).returns(Promise.resolve({ action: 'Purge the dead letter queue?' }));
+  prompt.onCall(1).returns(Promise.resolve({ purge: false }));
+
+  AWS.stub('CloudFormation', 'describeStacks', function() {
+    this.request.promise.returns(Promise.resolve({
+      Stacks: [
+        {
+          Outputs: [
+            { OutputKey: 'oneDeadLetterQueueUrl', OutputValue: 'oneDead' },
+            { OutputKey: 'oneQueueUrl', OutputValue: 'oneWork' },
+            { OutputKey: 'oneLogGroup', OutputValue: 'oneLogs' }
+          ]
+        }
+      ]
+    }));
+  });
+
+  const purge = AWS.stub('SQS', 'purgeQueue', function() {
+    this.request.promise.returns(Promise.resolve());
+  });
+
+  try {
+    await watchbotDeadletter();
+    assert.equal(purge.callCount, 0, 'does not call purgeQueue');
+
+    prompt.restore();
+    AWS.CloudFormation.restore();
+    AWS.SQS.restore();
+    assert.end();
+  } catch (err) {
+    assert.ifError(err, 'success');
+  }
+});
+
+test('[dead-letter] return messages to work queue', async (assert) => {
+  const prompt = sinon.stub(inquirer, 'prompt');
+  prompt.onCall(0).returns(Promise.resolve({ action: 'Return all dead messages to the work queue?' }));
+  prompt.onCall(1).returns(Promise.resolve({ replayAll: true }));
+
+  AWS.stub('CloudFormation', 'describeStacks', function() {
+    this.request.promise.returns(Promise.resolve({
+      Stacks: [
+        {
+          Outputs: [
+            { OutputKey: 'oneDeadLetterQueueUrl', OutputValue: 'oneDead' },
+            { OutputKey: 'oneQueueUrl', OutputValue: 'oneWork' },
+            { OutputKey: 'oneLogGroup', OutputValue: 'oneLogs' }
+          ]
+        }
+      ]
+    }));
+  });
+
+  let receives = 0;
+  const receive = AWS.stub('SQS', 'receiveMessage', function() {
+    receives++;
+    let payload = {};
+
+    if (receives === 1) payload = {
+      Messages: [
+        {
+          MessageId: 'id-1',
+          Body: JSON.stringify({ Subject: 'subject-1', Message: 'message-1' }),
+          ReceiptHandle: 'handle-1'
+        },
+        {
+          MessageId: 'id-2',
+          Body: JSON.stringify({ Subject: 'subject-2', Message: 'message-2' }),
+          ReceiptHandle: 'handle-2'
+        }
+      ]
+    };
+
+    if (receives > 1) payload = { Messages: [] };
+
+    this.request.promise.returns(Promise.resolve(payload));
+  });
+
+  const send = AWS.stub('SQS', 'sendMessage', function() {
+    this.request.promise.returns(Promise.resolve());
+  });
+
+  const del = AWS.stub('SQS', 'deleteMessage', function() {
+    this.request.promise.returns(Promise.resolve());
+  });
+
+  try {
+    await watchbotDeadletter();
+
+    assert.equal(prompt.args[1][0].type, 'confirm', 'second prompt type = confirm');
+
+    assert.equal(receive.callCount, 2, 'calls receiveMessage twice');
+    assert.ok(receive.alwaysCalledWith({
+      QueueUrl: 'oneDead',
+      WaitTimeSeconds: 1,
+      MaxNumberOfMessages: 10,
+      VisibilityTimeout: 600
+    }), 'reads correct queue, uses long-polling, receives up to 10, 10min timeout');
+
+    assert.equal(send.callCount, 2, 'calls sendMessage twice');
+    assert.ok(send.calledWith({
+      QueueUrl: 'oneWork',
+      MessageBody: JSON.stringify({ Subject: 'subject-1', Message: 'message-1' })
+    }), 'sends one dead SQS message back to work queue');
+    assert.ok(send.calledWith({
+      QueueUrl: 'oneWork',
+      MessageBody: JSON.stringify({ Subject: 'subject-2', Message: 'message-2' })
+    }), 'sends the other dead SQS message back to work queue');
+
+    assert.equal(del.callCount, 2, 'calls deleteMessage twice');
+    assert.ok(del.calledWith({
+      QueueUrl: 'oneDead',
+      ReceiptHandle: 'handle-1'
+    }), 'deletes one message from dead letter queue');
+    assert.ok(del.calledWith({
+      QueueUrl: 'oneDead',
+      ReceiptHandle: 'handle-2'
+    }), 'deletes the other message from dead letter queue');
+
+    prompt.restore();
+    AWS.CloudFormation.restore();
+    AWS.SQS.restore();
+    assert.end();
+  } catch (err) {
+    assert.ifError(err, 'success');
+  }
+});
+
+test('[dead-letter] reject return messages confirmation', async (assert) => {
+  const prompt = sinon.stub(inquirer, 'prompt');
+  prompt.onCall(0).returns(Promise.resolve({ action: 'Return all dead messages to the work queue?' }));
+  prompt.onCall(1).returns(Promise.resolve({ replayAll: false }));
+
+  AWS.stub('CloudFormation', 'describeStacks', function() {
+    this.request.promise.returns(Promise.resolve({
+      Stacks: [
+        {
+          Outputs: [
+            { OutputKey: 'oneDeadLetterQueueUrl', OutputValue: 'oneDead' },
+            { OutputKey: 'oneQueueUrl', OutputValue: 'oneWork' },
+            { OutputKey: 'oneLogGroup', OutputValue: 'oneLogs' }
+          ]
+        }
+      ]
+    }));
+  });
+
+  const receive = AWS.stub('SQS', 'receiveMessage', function() {
+    this.request.promise.returns(Promise.resolve());
+  });
+
+  const send = AWS.stub('SQS', 'sendMessage', function() {
+    this.request.promise.returns(Promise.resolve());
+  });
+
+  const del = AWS.stub('SQS', 'deleteMessage', function() {
+    this.request.promise.returns(Promise.resolve());
+  });
+
+  try {
+    await watchbotDeadletter();
+    assert.equal(receive.callCount, 0, 'receives no messages');
+    assert.equal(send.callCount, 0, 'sends no messages');
+    assert.equal(del.callCount, 0, 'deletes no messages');
+
+    prompt.restore();
+    AWS.CloudFormation.restore();
+    AWS.SQS.restore();
+    assert.end();
+  } catch (err) {
+    assert.ifError(err, 'success');
+  }
+});
+
+test('[dead-letter] write out messages', async (assert) => {
+  const prompt = sinon.stub(inquirer, 'prompt');
+  prompt.onCall(0).returns(Promise.resolve({ action: 'Print out all dead messages?' }));
+
+  AWS.stub('CloudFormation', 'describeStacks', function() {
+    this.request.promise.returns(Promise.resolve({
+      Stacks: [
+        {
+          Outputs: [
+            { OutputKey: 'oneDeadLetterQueueUrl', OutputValue: 'oneDead' },
+            { OutputKey: 'oneQueueUrl', OutputValue: 'oneWork' },
+            { OutputKey: 'oneLogGroup', OutputValue: 'oneLogs' }
+          ]
+        }
+      ]
+    }));
+  });
+
+  const receive = AWS.stub('SQS', 'receiveMessage');
+  receive.onCall(0).returns({
+    promise: () => Promise.resolve({
+      Messages: [
+        { MessageId: 'id-1', Body: JSON.stringify({ Subject: 'subject-1', Message: 'message-1' }), ReceiptHandle: 'handle-1' },
+        { MessageId: 'id-2', Body: JSON.stringify({ Subject: 'subject-2', Message: 'message-2' }), ReceiptHandle: 'handle-2' }
+      ]
+    })
+  });
+  receive.onCall(1).returns({
+    promise: () => Promise.resolve({})
+  });
+
+  const vis = AWS.stub('SQS', 'changeMessageVisibility', function() {
+    this.request.promise.returns(Promise.resolve());
+  });
+
+  try {
+    await watchbotDeadletter();
+
+    assert.equal(vis.callCount, 2, 'two changeMessageVisibility requests');
+    assert.ok(vis.calledWith({
+      QueueUrl: 'oneDead',
+      ReceiptHandle: 'handle-1',
+      VisibilityTimeout: 0
+    }), 'returns the first message to the dead letter queue');
+    assert.ok(vis.calledWith({
+      QueueUrl: 'oneDead',
+      ReceiptHandle: 'handle-2',
+      VisibilityTimeout: 0
+    }), 'returns the second message to the dead letter queue');
+
+    prompt.restore();
+    AWS.CloudFormation.restore();
+    AWS.SQS.restore();
+  } catch (err) {
+    assert.ifError(err, 'success');
+  } finally {
+    assert.end();
+  }
+});
