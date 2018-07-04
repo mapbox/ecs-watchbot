c6380adb47eef721b3a73da1fa5e26bb3e31cec7
arunasank <aruna@mapbox.com>
2018-07-04 14:11:39 +0530

    fix tests

--- test/bin.watchbot-binary-generator.test.js
+++ test/bin.watchbot-binary-generator.test.js
@@ -9,8 +9,7 @@ const exec = util.promisify(cp.exec);
 const sinon = require('sinon');
 
 test('getTagForSha: commit found', async (assert) => {
-  const execMock = sinon.stub(exec).callsFake((command) => {
-    console.log('called fake');
+  const execStub = sinon.stub(wbg, 'exec').callsFake((command) => {
     assert.equals(command, 'git ls-remote --tags https://github.com/mapbox/ecs-watchbot');
     return Promise.resolve({ stdout: 'f4815eb9f3bcfba88930bbe12d0888254af7cfa6\t/refs/tags/v4.1.1' });
   });
@@ -20,12 +19,18 @@ test('getTagForSha: commit found', async (assert) => {
   const tag = await wbg.getTagForSha(commit);
   assert.ok(tag, 'tag exists for this commit');
   assert.equals(tag, 'v4.1.1');
+  execStub.restore();
   assert.end();
 });
 
-test('getTagForSha: commit found', async (assert) => {
-  const commit = 'f4815eb9f3bcfba88930bbe12d0888254af7cfa6';
+test('getTagForSha: commit not found', async (assert) => {
+  const execStub = sinon.stub(wbg, 'exec').callsFake((command) => {
+    assert.equals(command, 'git ls-remote --tags https://github.com/mapbox/ecs-watchbot');
+    return Promise.resolve({ stdout: 'f4815eb9f3bcfba88930bbe12d0888254af7cfa6\t/refs/tags/v4.1.1' });
+  });
+
+  const commit = '123456';
   const tag = await wbg.getTagForSha(commit);
-  assert.ok(tag, 'tag exists for this commit');
+  assert.ok(!tag, 'tag does not exist for this commit');
   assert.end();
 });
