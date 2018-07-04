c812419e9c8fc00886f8baf1169874c7bff4c089
arunasank <aruna@mapbox.com>
2018-07-04 08:25:01 +0530

    add test

--- test/bin.watchbot-binary-generator.test.js
+++ test/bin.watchbot-binary-generator.test.js
@@ -3,9 +3,29 @@
 const test = require('tape');
 const wbg = require('../bin/watchbot-binary-generator');
 const AWS = require('@mapbox/mock-aws-sdk-js');
-const lib = require('../bin/watchbot-binary-generator.js');
+const cp = require('child_process');
+const util = require('util');
+const exec = util.promisify(cp.exec);
+const sinon = require('sinon');
 
-tape('getTagForSha: commit found', async (assert) => {
-	const tagSha = 'f4815eb9f3bcfba88930bbe12d0888254af7cfa6';
-	const
+test('getTagForSha: commit found', async (assert) => {
+  const execMock = sinon.stub(exec).callsFake((command) => {
+    console.log('called fake');
+    assert.equals(command, 'git ls-remote --tags https://github.com/mapbox/ecs-watchbot');
+    return Promise.resolve({ stdout: 'f4815eb9f3bcfba88930bbe12d0888254af7cfa6\t/refs/tags/v4.1.1' });
+  });
+
+  const commit = 'f4815eb9f3bcfba88930bbe12d0888254af7cfa6';
+
+  const tag = await wbg.getTagForSha(commit);
+  assert.ok(tag, 'tag exists for this commit');
+  assert.equals(tag, 'v4.1.1');
+  assert.end();
+});
+
+test('getTagForSha: commit found', async (assert) => {
+  const commit = 'f4815eb9f3bcfba88930bbe12d0888254af7cfa6';
+  const tag = await wbg.getTagForSha(commit);
+  assert.ok(tag, 'tag exists for this commit');
+  assert.end();
 });
