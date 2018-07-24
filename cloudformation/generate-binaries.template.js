9ef4c82218605b57d401d1e7e4b1dda46357ccad
arunasank <aruna@mapbox.com>
2018-07-24 16:10:59 +0530

    change branch

--- cloudformation/generate-binaries.template.js
+++ cloudformation/generate-binaries.template.js
@@ -140,7 +140,7 @@ const Resources = {
       ServiceToken: cf.importValue('code-pipeline-helper-production-custom-resource'),
       Owner: 'mapbox',
       Repo: 'ecs-watchbot',
-      Branch: 'master',
+      Branch: 'binary-use-tags',
       Name: cf.stackName,
       RoleArn: cf.getAtt('PipelineRole', 'Arn'),
       ArtifactStore: {
