cd80bbb896c6ba37536de8f868d63f67857c0bdf
Ryan Clark <ryan.clark.j@gmail.com>
2018-08-08 18:55:12 -0700

    try out fifo queues

--- test/template.spec.js
+++ test/template.spec.js
@@ -55,4 +55,15 @@ test('[template]', () => {
   }));
 
   expect(setsAllOptions).toMatchSnapshot('all-properties');
+
+  const fifo = cf.merge(template({
+    service: 'example',
+    serviceVersion: '1',
+    command: 'echo hello world',
+    cluster: 'processing',
+    notificationEmail: 'hello@mapbox.pagerduty.com',
+    fifo: true
+  }));
+
+  expect(fifo).toMatchSnapshot('fifo');
 });
