c78de1297b443853b835a9b79cd9157b47d8ac03
Xtreme Love - We love extreme <132666188+xtreme-love@users.noreply.github.com>
2023-05-05 16:37:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = {
-  progress: require('@mapbox/watchbot-progress').progress,
-  template: require('./lib/template')
+  template: require('./lib/template'),
+  progress: require('@mapbox/watchbot-progress').progress
 };
