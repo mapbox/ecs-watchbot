8afbc216a41a5eb03cad9ed941e0c2621adca2da
oielbanna <>
2023-12-13 15:04:53 -0500

    intermediate commit

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -4,26 +4,23 @@ import {Construct} from "constructs";
 import {
     App,
     aws_codebuild,
-    aws_codepipeline,
-    BootstraplessSynthesizer,
+    aws_codepipeline, aws_codestar, aws_codestarconnections,
+    BootstraplessSynthesizer, CliCredentialsStackSynthesizer,
     Duration,
     RemovalPolicy,
-    SecretValue,
     Stack,
-    StackProps
+    StackProps,
 } from "aws-cdk-lib";
-import {CodePipelineSource} from "aws-cdk-lib/pipelines";
 import {Bucket, BucketEncryption} from "aws-cdk-lib/aws-s3";
-import {CodeBuildAction, GitHubSourceAction} from "aws-cdk-lib/aws-codepipeline-actions";
-import {Artifact} from "aws-cdk-lib/aws-codepipeline";
-import {BuildSpec} from "aws-cdk-lib/aws-codebuild";
+import {BuildSpec, BuildEnvironmentVariableType} from "aws-cdk-lib/aws-codebuild";
 import {LogGroup, RetentionDays} from "aws-cdk-lib/aws-logs";
+import {Effect, PolicyDocument, PolicyStatement, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
 
 const app = new App();
 
-const input =  CodePipelineSource.connection('mapbox/ecs-watchbot', 'master', {
-    connectionArn: 'arn:aws:codestar-connections:us-east-1:222222222222:connection/7d2469ff-514a-4e4f-9003-5ca4a43cdc41', // Created using the AWS console * });', TODO update
-});
+// const input =  CodePipelineSource.connection('mapbox/ecs-watchbot', 'master', {
+//     connectionArn: 'arn:aws:codestar-connections:us-east-1:353802256504:connection/da887df2-781b-4a00-8dce-24dced5cc129',
+// });
 
 interface Props extends StackProps {
     deploymentEnvironment: string
@@ -40,94 +37,185 @@ class PipelineStack extends Stack {
             removalPolicy: RemovalPolicy.DESTROY,
         });
 
-        const artifact = new Artifact('Source');
-        const p = new aws_codepipeline.Pipeline(this, 'Pipeline', {
-            pipelineName: this.stackName,
-            artifactBucket: Bucket.fromBucketName(this, 'Bucket', props.bucketName),
-            stages: [{
-                stageName: 'Source',
-                actions: [
-                    new GitHubSourceAction({
-                        actionName: 'Github',
-                        branch: 'master',
-                        owner: 'mapbox',
-                        repo: 'ecs-watchbot',
-                        output: artifact,
-                        oauthToken: SecretValue.secretsManager('ecs-watchbot/code-pipeline-github-token')
-                    }),
-                ]
-            }, {
-              stageName: 'Bundle',
-              actions: [
-                  new CodeBuildAction({
-                      actionName: 'LinuxBundle',
-                      input: artifact,
-                      project: new aws_codebuild.PipelineProject(this, 'LinuxBundle', {
-                          environment: {
-                              buildImage: aws_codebuild.LinuxBuildImage.STANDARD_7_0
-                          },
-                          logging: {
-                            cloudWatch: {
-                                enabled: true,
-                                logGroup: codebuildLogGroup,
-                            }
-                          },
-                          projectName: `${this.stackName}-linux-bundle`,
-                          buildSpec: BuildSpec.fromObject({
-                              version: '0.2',
-                              phases: {
-                                  install: {
-                                      'commands': [
-                                          'npm ci --production'
-                                      ]
-                                  },
-                                  build: {
-                                      commands:[
-                                          'node bin/watchbot-binary-generator',
-                                      ],
-                                  },
-                              },
-                          })
-                      })
-                  }),
-                  new CodeBuildAction({
-                      actionName: 'AlpineBundle',
-                      input: artifact,
-                      project: new aws_codebuild.PipelineProject(this, 'AlpineBundle', {
-                          environment: {
-                              buildImage: aws_codebuild.LinuxBuildImage.fromCodeBuildImageId('public.ecr.aws/docker/library/node:18-alpine')
-                          },
-                          logging: {
-                              cloudWatch: {
-                                  enabled: true,
-                                  logGroup: codebuildLogGroup,
-                              }
-                          },
-                          projectName: `${this.stackName}-alpine-bundler`,
-                          description: 'Builds watchbot binaries for alpine OS',
-                          buildSpec: BuildSpec.fromObject({
-                              version: '0.2',
-                              phases: {
-                                  install: {
-                                      'commands': [
-                                          'apk add git',
-                                          'npm ci --production'
-                                      ]
-                                  },
-                                  build: {
-                                      commands:[
-                                          'node bin/watchbot-binary-generator alpine',
-                                      ],
-                                  },
-                              },
-                          })
-                      })
-                  })
-              ]
-            }]
+        const linuxCodebuild = new aws_codebuild.PipelineProject(this, 'LinuxBundle', {
+            environment: {
+                buildImage: aws_codebuild.LinuxBuildImage.STANDARD_7_0
+            },
+            logging: {
+                cloudWatch: {
+                    enabled: true,
+                    logGroup: codebuildLogGroup,
+                }
+            },
+            environmentVariables: {
+                BUCKET_NAME: {
+                    type: BuildEnvironmentVariableType.PLAINTEXT,
+                    value: props.bucketName
+                }
+            },
+            projectName: `${this.stackName}-linux-bundle`,
+            buildSpec: BuildSpec.fromObject({
+                version: '0.2',
+                phases: {
+                    install: {
+                        'commands': [
+                            'npm ci --production'
+                        ]
+                    },
+                    build: {
+                        commands:[
+                            'node bin/watchbot-binary-generator',
+                        ],
+                    },
+                },
+            })
+        });
+        const alpineCodebuild = new aws_codebuild.PipelineProject(this, 'AlpineBundle', {
+            environment: {
+                buildImage: aws_codebuild.LinuxBuildImage.fromCodeBuildImageId('public.ecr.aws/docker/library/node:18-alpine')
+            },
+            logging: {
+                cloudWatch: {
+                    enabled: true,
+                    logGroup: codebuildLogGroup,
+                }
+            },
+            environmentVariables: {
+                BUCKET_NAME: {
+                    type: BuildEnvironmentVariableType.PLAINTEXT,
+                    value: props.bucketName
+                }
+            },
+            projectName: `${this.stackName}-alpine-bundler`,
+            description: 'Builds watchbot binaries for alpine OS',
+            buildSpec: BuildSpec.fromObject({
+                version: '0.2',
+                phases: {
+                    install: {
+                        'commands': [
+                            'apk add git',
+                            'npm ci --production'
+                        ]
+                    },
+                    build: {
+                        commands:[
+                            'node bin/watchbot-binary-generator alpine',
+                        ],
+                    },
+                },
+            })
         })
+        const role = new Role(this, 'PipelineRole', {
+            assumedBy: new ServicePrincipal('codepipeline.amazonaws.com'),
+            inlinePolicies: {
+                main: new PolicyDocument({
+                    statements: [new PolicyStatement({
+                        sid: 'S3permissions',
+                        effect: Effect.ALLOW,
+                        actions: [
+                            's3:ListBucket',
+                            's3:GetBucketVersioning',
+                            's3:GetObject',
+                            's3:GetObjectVersion',
+                            's3:PutObject*'
+                        ],
+                        resources: [
+                            `arn:aws:s3:::${props.bucketName}`,
+                            `arn:aws:s3:::${props.bucketName}/*`,
+                        ]
+                    }), new PolicyStatement({
+                        effect: Effect.ALLOW,
+                        actions: [
+                            'codebuild:StartBuild',
+                            'codebuild:BatchGetBuilds',
+                        ],
+                        resources: ['*']
+                    }), new PolicyStatement({
+                        effect: Effect.ALLOW,
+                        actions: [
+                            'sts:AssumeRole'
+                        ],
+                        resources: [
+                            linuxCodebuild.role?.roleArn || '',
+                            alpineCodebuild.role?.roleArn  || '',
+                        ]
+                    })]
+                })
+            }
+        });
+
+        /*
+         aws_codepipeline.Pipeline creates v1 which can't be re-triggered with overrides and that's needed for testing
+         codepipeline.CodePipeline creates v2 but forces you to have a stack
+        */
+        const connection = new aws_codestarconnections.CfnConnection(this, 'Connection', {
+            connectionName: 'ecs-watchbot',
+            providerType: 'GitHub'
+        });
+        new aws_codepipeline.CfnPipeline(this, 'Pipeline', {
+            roleArn: role.roleArn,
+            name: this.stackName,
+
+            stages: [{
+                name: 'Source',
+                actions: [{
+                    name: 'Github',
+                    actionTypeId: {
+                        category: 'Source',
+                        owner: 'AWS',
+                        provider: 'CodeStarSourceConnection',
+                        version: '1',
+                    },
+                    configuration: {
+                        ConnectionArn: connection.attrConnectionArn, // 'arn:aws:codestar-connections:us-east-1:353802256504:connection/da887df2-781b-4a00-8dce-24dced5cc129',
+                        FullRepositoryId: 'mapbox/ecs-watchbot',
+                        BranchName: 'master',
+                        // oAuthToken: '{{resolve:secretsmanager:ecs-watchbot/code-pipeline-github-token:SecretString:::}}'
+                    },
+                    outputArtifacts: [{
+                        name: 'Source',
+                    }],
+                }],
+            }, {
+                name: 'Bundle',
+                actions: [{
+                    name: 'Bundle',
+                    actionTypeId: {
+                        category: 'Build',
+                        owner: 'AWS',
+                        provider: 'CodeBuild',
+                        version: '1',
+                    },
+                    inputArtifacts: [{
+                        name: 'Source'
+                    }],
+                    configuration: {
+                        ProjectName: linuxCodebuild.projectName
+                    }
+                }, {
+                    name: 'AlpineBundle',
+                    actionTypeId: {
+                        category: 'Build',
+                        owner: 'AWS',
+                        provider: 'Codebuild',
+                        version: '1',
+                    },
+                    inputArtifacts: [{
+                        name: 'Source'
+                    }],
+                    configuration: {
+                        ProjectName: alpineCodebuild.projectName
+                    }
+                }]
+            }],
+            artifactStore: {
+                location: props.bucketName,
+                type: 'S3',
+            },
+        });
     }
 }
+
 
 class BucketStack extends Stack {
     constructor(scope: Construct, id: string, props: Props) {
@@ -168,10 +256,15 @@ const bucketName = isProduction(deploymentEnvironment) ? 'ecs-watchbot-binaries'
 const pipelineStackName = 'watchbot-pipeline';
 new PipelineStack(app, 'Pipeline', {
     stackName: pipelineStackName,
-    synthesizer: new BootstraplessSynthesizer({
-        cloudFormationExecutionRoleArn: process.env.AWS_CDK_EXEC_ROLE,
-        deployRoleArn: process.env.AWS_CDK_DEPLOY_ROLE,
+    synthesizer: new CliCredentialsStackSynthesizer({ // TODO give cdk roles codestar-connections:* actions
+       fileAssetsBucketName: `cdk-assets-${account}-${region}`,
+        bucketPrefix: `ecs-watchbot/${pipelineStackName}`,
+       qualifier: 'operator'
     }),
+    // synthesizer: new BootstraplessSynthesizer({
+    //     cloudFormationExecutionRoleArn: process.env.AWS_CDK_EXEC_ROLE,
+    //     deployRoleArn: process.env.AWS_CDK_DEPLOY_ROLE,
+    // }),
     env: { account, region },
     tags: {
         ...tags,
@@ -197,4 +290,4 @@ new BucketStack(app, 'Bucket', {
     },
     deploymentEnvironment: deploymentEnvironment,
     bucketName,
-})
+});
