744b57b15d3ca1f7428310355250b9e289d22d61
oielbanna <>
2023-12-14 12:08:53 -0500

    change to BootstraplessSynthesizer (needed for production account)

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import {
-    App,
-    CliCredentialsStackSynthesizer,
+    App, BootstraplessSynthesizer
 } from "aws-cdk-lib";
 import {BucketStack} from "../cdk/BucketStack";
 import {PipelineStack} from "../cdk/PipelineStack";
@@ -24,10 +23,9 @@ const bucketName = isProduction(deploymentEnvironment) ? 'ecs-watchbot-binaries'
 const pipelineStackName = 'watchbot-pipeline';
 new PipelineStack(app, 'Pipeline', {
     stackName: pipelineStackName,
-    synthesizer: new CliCredentialsStackSynthesizer({
-       fileAssetsBucketName: `cdk-assets-${account}-${region}`,
-        bucketPrefix: `ecs-watchbot/${pipelineStackName}`,
-       qualifier: 'operator'
+    synthesizer: new BootstraplessSynthesizer({
+        deployRoleArn: `arn:aws:iam::${account}:role/CdkDeployOperatorRole`,
+        cloudFormationExecutionRoleArn: `arn:aws:iam::${account}:role/CdkExecOperatorRole`
     }),
     env: { account, region },
     tags: {
@@ -42,10 +40,9 @@ new PipelineStack(app, 'Pipeline', {
 const bucketStackName = 'watchbot-bucket';
 new BucketStack(app, 'Bucket', {
     stackName: bucketStackName,
-    synthesizer: new CliCredentialsStackSynthesizer({
-        fileAssetsBucketName: `cdk-assets-${account}-${region}`,
-        bucketPrefix: `ecs-watchbot/${bucketStackName}`,
-        qualifier: 'operator'
+    synthesizer: new BootstraplessSynthesizer({
+        deployRoleArn: `arn:aws:iam::${account}:role/CdkDeployOperatorRole`,
+        cloudFormationExecutionRoleArn: `arn:aws:iam::${account}:role/CdkExecOperatorRole`
     }),
     env: { account, region },
     tags: {
