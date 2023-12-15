fca756876c97c2aebee68a5825e87d14063c11ea
Omar Ibrahim <omar.ibrahim@mapbox.com>
2023-12-15 11:02:07 -0500

    Build Pipeline (#389)
    * add pipeline
    
    * add bucket stack
    
    * deploy staging bucket
    
    * deploy pipeline stg
    
    * intermediate commit
    
    * use BUCKET_NAME env variable
    
    * add required permissions
    
    * 10.0.0-0
    
    * dont use ACLs
    
    * 10.0.0-1
    
    * add prerelease script
    
    * 10.0.0-2
    
    * add logging to script
    
    * 10.0.0-3
    
    * add start pipeline execution part
    
    * 10.0.0-4
    
    * update branchName first
    
    * 10.0.0-5
    
    * add regions
    
    * 10.0.0-6
    
    * fix error
    
    * 10.0.0-7
    
    * increment aws-sdk and add triggers
    
    * 10.0.0-8
    
    * 10.0.0-9
    
    * 10.0.0-10
    
    * 10.0.0-11
    
    * last test
    
    * 10.0.0-12
    
    * last test
    
    * 10.0.0-13
    
    * cleanup code
    add tests stage to pipeline
    
    * fix test codebuild permissions
    
    * 10.0.0-14
    
    * PR comments
    Update CONTRIBUTING.md
    remove Artifacts and buildspec.yml (no more codebuild yay)
    
    * change to BootstraplessSynthesizer (needed for production account)
    
    * fix BranchName
    
    * fix bucket access
    
    * fix bucket access
    
    ---------
    
    Co-authored-by: oielbanna <>

--- bin/watchbot-binary-generator.js
+++ bin/watchbot-binary-generator.js
@@ -38,7 +38,11 @@ wbg.getTagForSha = getTagForSha;
  */
 const uploadBundle = async (buildTarget) => {
   const s3 = new AWS.S3();
-  const Bucket = 'watchbot-binaries';
+  const Bucket = process.env.BUCKET_NAME;
+  if (!Bucket) {
+    throw new Error('BUCKET_NAME environment variable missing');
+  }
+  console.log(`Publishing artifacts to bucket=${Bucket}`);
 
   let targets = [
     { prefix: 'linux', target: 'node18-linux', pkg: 'watchbot-linux' },
@@ -64,8 +68,7 @@ const uploadBundle = async (buildTarget) => {
       return s3.putObject({
         Bucket,
         Key: `${target.prefix}/${tag}/watchbot`,
-        Body: fs.createReadStream(target.pkg),
-        ACL: 'public-read'
+        Body: fs.createReadStream(target.pkg)
       }).promise();
     });
 
---
+++ cdk/BucketStack.ts
@@ -0,0 +1,58 @@
+import {aws_s3, Duration, Stack, StackProps} from "aws-cdk-lib";
+import {Construct} from "constructs";
+import {BlockPublicAccess, Bucket, BucketEncryption} from "aws-cdk-lib/aws-s3";
+import {isProduction} from "./util";
+import {AnyPrincipal, Effect, PolicyStatement} from "aws-cdk-lib/aws-iam";
+
+interface Props extends StackProps {
+    deploymentEnvironment: string
+    bucketName: string
+}
+
+export class BucketStack extends Stack {
+    constructor(scope: Construct, id: string, props: Props) {
+        super(scope, id, props);
+
+        const bucket = new Bucket(this, 'Bucket', {
+            bucketName: props.bucketName,
+            blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+            enforceSSL: true,
+            versioned: true,
+            encryption: BucketEncryption.S3_MANAGED,
+            lifecycleRules: [{
+                enabled: true,
+                id: 'AbortIncompleteMultipartUploads',
+                abortIncompleteMultipartUploadAfter: Duration.days(1),
+            }, {
+                enabled: true,
+                id: 'ExpireNonCurrentVersions',
+                noncurrentVersionExpiration: Duration.days(1),
+            }]
+        });
+
+        bucket.addToResourcePolicy(new PolicyStatement({
+            actions: ['s3:GetObject'],
+            resources: [
+                bucket.arnForObjects('linux/*'),
+                bucket.arnForObjects('macosx/*'),
+                bucket.arnForObjects('windows/*'),
+                bucket.arnForObjects('alpine/*'),
+            ],
+            principals: [new AnyPrincipal()],
+            effect: Effect.ALLOW,
+            conditions: {
+                'Bool': {
+                    'aws:SecureTransport': 'true',
+                },
+            },
+        }));
+
+        if (!isProduction(props.deploymentEnvironment)) { // delete test versions only
+            bucket.addLifecycleRule({
+                expiration: Duration.days(14)
+            });
+        }
+
+    }
+
+}
