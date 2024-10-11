585c827b083417a061b753dcee91504a4211a53f
mappingvermont <charlie.hofmann@gmail.com>
2024-10-11 16:05:15 -0400

    more step tweaks

--- lib/MapboxQueueProcessingFargateService.ts
+++ lib/MapboxQueueProcessingFargateService.ts
@@ -220,7 +220,8 @@ export class MapboxQueueProcessingFargateService extends QueueProcessingServiceB
     scaling.scaleOnMetric('TotalMessagesScaling', {
       metric: this.totalMessagesMetric,
       scalingSteps: [
-        { lower: 0, upper: 0, change: -100 }
+        { lower: -2, upper: -1, change: -100 }, // this is a bogus param - we require two for autoscaling
+        { lower: -1, upper: 0, change: -100 },
       ],
       evaluationPeriods: 3
     })
@@ -228,7 +229,8 @@ export class MapboxQueueProcessingFargateService extends QueueProcessingServiceB
     scaling.scaleOnMetric('VisibleMessagesScaling', {
       metric: this.visibleMessagesMetric,
       scalingSteps: [
-        { lower: 1, upper: 1000000, change: 1 },
+        { lower: 10, upper: 10000000, change: 5 }, // test - let's add 5 tasks if there are > 10 messages
+        { lower: 1, upper: 10, change: 1 },
       ],
       evaluationPeriods: 3
     })
